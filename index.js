7e7f26b7967f142a2e108380bb022587dce076e4
Bartek <bartekma89@gmail.com>
2018-01-28 14:19:31 +0100

    added bootstrap components, basic logic to show information about movie

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-var movieElements = movies.map(function(movie) {
-    return React.createElement('li', {key:movie.Id},
-        React.createElement('h2', {}, movie.Title),
-        React.createElement('p', {}, movie.Plot)
+var index = 0;
+var data_movies = movies[index];
+
+var movie = function(data) {
+    return React.createElement('div', { className: 'media border' },
+        React.createElement('div', { className: 'media-left' },
+            React.createElement('img', {
+                className: 'media-object',
+                alt: 'Pic of movie',
+                src: data.Images[0],
+                height: 200,
+                width: 300
+            })
+        ),
+        React.createElement('div', { className: 'media-body' },
+            React.createElement('h2', { className: 'media-heading' }, data.Title),
+            React.createElement('p', {}, data.Plot)
+        )
     )
-});
+}
 
-var element = React.createElement('div', {},
-    React.createElement('h1', {}, 'Lista filmow'),
-    React.createElement('ul', {}, movieElements)
-);
-
-ReactDOM.render(element, document.getElementById('app'));
+ReactDOM.render(movie(data_movies), document.getElementById('app'));
