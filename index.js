8cca62ef54f4be11061fd521f544d53fcc316a3c
Bartek <bartekma89@gmail.com>
2018-01-28 21:26:06 +0100

    new solution

--- index.js
+++ index.js
@@ -1,42 +1,23 @@
-var index = 0;
-var data_movies = movies[index];
-
-var movie = function(data) {
-    return React.createElement('div', { className: 'media border', key:data.Id },
+var movie = movies.map(function(movie) {
+    return React.createElement('div', { className: 'media border', key: movie.Id },
         React.createElement('div', { className: 'media-left' },
             React.createElement('img', {
                 className: 'media-object',
                 alt: 'Pic of movie',
-                src: data.Images[random(data)],
+                src: movie.Images[1],
                 height: 200,
                 width: 300
             })
         ),
         React.createElement('div', { className: 'media-body' },
-            React.createElement('h2', { className: 'media-heading' }, data.Title),
-            React.createElement('p', {}, data.Plot)
+            React.createElement('h2', { className: 'media-heading' }, movie.Title),
+            React.createElement('p', {}, movie.Plot)
         )
     )
-}
+})
 
 render();
 
-document.getElementById('next').addEventListener('click', function() {
-    (index === movies.length - 1)? index = movies.length - 1: ++index;
-    data_movies = movies[index];
-    render();
-})
-
-document.getElementById('prev').addEventListener('click', function() {
-    (index === 0) ? index = 0: --index;
-    data_movies = movies[index];
-    render();
-})
-
 function render() {
-    ReactDOM.render(movie(data_movies), document.getElementById('app'));
-}
-
-function random(data) {
-    return Math.floor(Math.random() * data.Images.length);
+    ReactDOM.render(movie, document.getElementById('app'));
 }
