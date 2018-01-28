46acc6b8c0722cfbbd2c508bb77e2731dc4dff64
Bartek <bartekma89@gmail.com>
2018-01-28 14:45:22 +0100

    added all features

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var movie = function(data) {
             React.createElement('img', {
                 className: 'media-object',
                 alt: 'Pic of movie',
-                src: data.Images[0],
+                src: data.Images[random(data)],
                 height: 200,
                 width: 300
             })
@@ -19,4 +19,24 @@ var movie = function(data) {
     )
 }
 
-ReactDOM.render(movie(data_movies), document.getElementById('app'));
+render();
+
+document.getElementById('next').addEventListener('click', function() {
+    (index === movies.length - 1)? index = movies.length - 1: ++index;
+    data_movies = movies[index];
+    render();
+})
+
+document.getElementById('prev').addEventListener('click', function() {
+    (index === 0) ? index = 0: --index;
+    data_movies = movies[index];
+    render();
+})
+
+function render() {
+    ReactDOM.render(movie(data_movies), document.getElementById('app'));
+}
+
+function random(data) {
+    return Math.floor(Math.random() * data.Images.length);
+}
